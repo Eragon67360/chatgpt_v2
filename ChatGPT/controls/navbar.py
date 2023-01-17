34f287c58158b24b7690cc71a2eaf91ad1e15987
Thomas Moser <thomas.moser@bmglabtech.com>
2023-01-17 11:43:13 +0100

    Changed icons, added functions  slots

--- ChatGPT/controls/navbar.py
+++ ChatGPT/controls/navbar.py
@@ -5,15 +5,22 @@ Navbar menu
 from flet import *
 
 class ModernNavBar(UserControl):
-    def __init__(self):
+    def __init__(self, func_home, func_mode, func_discord, func_updates, func_profile, func_logout):
+        self.func_home = func_home
+        self.func_mode = func_mode
+        self.func_discord = func_discord
+        self.func_updates = func_updates
+        self.func_profile = func_profile
+        self.func_logout = func_logout
+
         super().__init__()
 
-    def ContainedIcon(self, icon_name, text):
+    def ContainedIcon(self, icon_name, text, func):
         return Container(
             width=180,
             height=45,
             border_radius=10,
-            on_click=None,
+            on_click=func,
             on_hover=None,
             content=Row(
                 controls=[
@@ -49,12 +56,14 @@ class ModernNavBar(UserControl):
                 horizontal_alignment=CrossAxisAlignment.START,
                 spacing=5,
                 controls=[
-                    self.ContainedIcon(icons.HOME_FILLED, 'Home'),
-                    self.ContainedIcon(icons.SUNNY, 'Light mode'),
+                    self.ContainedIcon(icons.HOME_FILLED, 'Home', self.func_home),
+                    self.ContainedIcon(icons.SUNNY, 'Light mode', self.func_mode),
                     Divider(color=colors.BLUE_GREY, height=5),
-                    self.ContainedIcon(icons.DISCORD, 'OpenAI Discord'),
-                    self.ContainedIcon(icons.OPEN_IN_NEW, 'Updates & FAQ'),
-                    self.ContainedIcon(icons.LOGOUT, 'Logout'),
+                    self.ContainedIcon(icons.DISCORD, 'OpenAI Discord', self.func_discord),
+                    self.ContainedIcon(icons.OPEN_IN_NEW, 'Updates & FAQ', self.func_updates),
+                    Divider(color=colors.BLUE_GREY, height=5),
+                    self.ContainedIcon(icons.PERSON, 'Profile', self.func_profile),
+                    self.ContainedIcon(icons.LOGOUT, 'Logout', self.func_logout),
                 ],
 
             )
