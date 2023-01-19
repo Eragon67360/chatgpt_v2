f3ab0ecafae7359a1818e2ab487c46659d35bc2e
Thomas Moser <thomas.moser@bmglabtech.com>
2023-01-19 17:28:02 +0100

    Added temperature and amount tokens as variable parameter

--- ChatGPT/controls/gpt.py
+++ ChatGPT/controls/gpt.py
@@ -53,8 +53,7 @@ class GPTWrite(UserControl):
                             max_lines=4,
                             expand=True,
                             shift_enter=True,
-                            on_submit=lambda e: self.onRequest(e),
-                            
+                            on_submit=lambda e: self.onRequest(e),                            
                         ),
                     ),
                     ProgressRing(
@@ -93,47 +92,49 @@ class GPTWrite(UserControl):
                 input_field = page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.controls[0].content
                 input = input_field.value
 
-
-                # Make progress ring visible
-                page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.controls[1].visible = True
-                page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.update()
-
-                answer_container = GPTAnswer()
-                answer_container.container.content.controls[0].content.controls[0].value = input
-                output_field = answer_container.container.content.controls[1].content.controls[2]
-                
-                display.append(answer_container)
-                page.controls[0].controls[0].controls[2].controls[1].update()
-
-                prompt = input + "(generate your answer using markdown)"
-                self.list_questions.append(input)
-                input_field.value = ""
-                input_field.read_only = True
-                input_field.update()
-                
-                if model == None:
-                    model = self.model_davinci
-                ret = getCompletion(model=model, prompt=prompt)
-                
-                if ret != None:
-                    self.list_answers.append(ret)
-                    page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.controls[1].visible = False
+                if input == "":
+                    e.page.show_snack_bar(SnackBar(content=Text("Please insert text", color=colors.RED), open=True, bgcolor="#474855"))
+                else:
+                    # Make progress ring visible
+                    page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.controls[1].visible = True
                     page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.update()
 
-                    text_outputed =  ""
-                    for i in ret:
-                        text_outputed = text_outputed + i
-                        output_field.value = text_outputed
-                        output_field.update()
-                        time.sleep(0.01)
+                    answer_container = GPTAnswer()
+                    answer_container.container.content.controls[0].content.controls[0].value = input
+                    output_field = answer_container.container.content.controls[1].content.controls[2]
+                    
+                    display.append(answer_container)
+                    page.controls[0].controls[0].controls[2].controls[1].update()
 
-                    input_field.read_only = False
+                    prompt = input + "(generate your answer using markdown)"
+                    self.list_questions.append(input)
+                    input_field.value = ""
+                    input_field.read_only = True
                     input_field.update()
-                    answer_container.container.content.controls[1].content.padding = 15
-                    answer_container.container.content.update()
-                    answer_container.container.content.controls[0].update()
-                    page.controls[0].controls[0].controls[2].controls[0].update()
-                    page.update()
+                    
+                    if model == None:
+                        model = self.model_davinci
+                    ret = getCompletion(model=model, prompt=prompt)
+                    
+                    if ret != None:
+                        self.list_answers.append(ret)
+                        page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.controls[1].visible = False
+                        page.controls[0].controls[0].controls[2].controls[1].controls[1].container.content.update()
+
+                        text_outputed =  ""
+                        for i in ret:
+                            text_outputed = text_outputed + i
+                            output_field.value = text_outputed
+                            output_field.update()
+                            time.sleep(0.01)
+
+                        input_field.read_only = False
+                        input_field.update()
+                        answer_container.container.content.controls[1].content.padding = 15
+                        answer_container.container.content.update()
+                        answer_container.container.content.controls[0].update()
+                        page.controls[0].controls[0].controls[2].controls[0].update()
+                        page.update()
 
 class GPTAnswer(UserControl):
     def __init__(self):
@@ -202,7 +203,41 @@ class GPTParameters(UserControl):
         for i in range(len(modelIDS)):
             options[i] = dropdown.Option(modelIDS[i])
         self.dropdown.options = options
-        
+        # temperature=0, max_tokens=2048,top_p=1, frequency_penalty=0.0, presence_penalty=0.0
         return Container(
-            content=self.dropdown,
+            Row(
+                alignment=MainAxisAlignment.SPACE_BETWEEN,
+                controls=[
+                    self.dropdown,
+                    Column(
+                        alignment=MainAxisAlignment.CENTER,
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                        controls=[                            
+                            Text("Temperature", size=15),
+                            Slider(
+                                active_color="#75a99c",
+                                min=0,
+                                max=1.0,
+                                divisions=10,
+                                label="{value}", 
+                                ),
+                        ]
+                    ),
+                    Column(
+                        alignment=MainAxisAlignment.CENTER,
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                        controls=[                            
+                            Text("Max tokens", size=15),
+                            Slider(
+                                active_color="#75a99c",
+                                min=1,
+                                max=2049,
+                                divisions=2048,
+                                label="{value}", 
+                                ),
+                        ]
+                    ),
+                ]
+            )
+            
         )
